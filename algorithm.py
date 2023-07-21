fe91b6f502f7af8957faa9c88f712212fafadf3b
vukhanh732 <vukhanh732@DESKTOP-HVJUMOE>
2023-07-21 20:58:56 +1000

    Added DFS and Dijkstra algorithm

--- algorithm.py
+++ algorithm.py
@@ -1,4 +1,5 @@
 from collections import deque
+import heapq
 
 def get_neighbors(maze, x, y):
     """
@@ -6,6 +7,7 @@ def get_neighbors(maze, x, y):
     """
     directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]  # Right, down, left, up
     neighbors = []
+
     for dx, dy in directions:
         nx, ny = x + dx, y + dy
         if 0 <= nx < maze.width and 0 <= ny < maze.height and not maze.maze[ny][nx].wall:
@@ -17,26 +19,98 @@ def solve_bfs(maze):
     """
     Solves the given maze using the Breadth-First Search algorithm.
     """
-    queue = deque([(maze.start, [])])
-    maze.maze[maze.start[1]][maze.start[0]].visited = True  # Visited
+    queue = deque([maze.start])
+    found = False 
 
     while queue:
-        (x, y), path = queue.popleft()
+        x, y = queue.popleft()
 
         if (x, y) == maze.end:
-            yield path + [(x, y)]  # Found a path
-            return
+            found = True 
+            break  
 
         for nx, ny in get_neighbors(maze, x, y):
             if not maze.maze[ny][nx].visited:  # Unvisited
-                queue.append(((nx, ny), path + [(x, y)]))
+                queue.append((nx, ny))
                 maze.maze[ny][nx].visited = True  # Mark as visited
+                maze.maze[ny][nx].parent = x, y
 
-        yield None  # Not yet found a path
+        yield False  # Not yet found a path
 
-    yield []  # No path found
+    # If a path is found, trace the path
+    if found:
+        x, y = maze.end
+        while (x, y) != maze.start:
+            maze.maze[y][x].path = True
+            x, y = maze.maze[y][x].parent
+        maze.maze[maze.start[1]][maze.start[0]].path = True
+
+    yield found
 
 
+def solve_dfs(maze):
+    """
+    Solves the given maze using the Depth-First Search algorithm.
+    """
+    stack = [maze.start]
+    found = False 
 
+    while stack:
+        x, y = stack.pop()
 
+        if (x, y) == maze.end:
+            found = True 
+            break  
 
+        for nx, ny in get_neighbors(maze, x, y):
+            if not maze.maze[ny][nx].visited:  # Unvisited
+                stack.append((nx, ny))
+                maze.maze[ny][nx].visited = True  # Mark as visited
+                maze.maze[ny][nx].parent = x, y
+
+        yield False  # Not yet found a path
+
+    # If a path is found, trace the path
+    if found:
+        x, y = maze.end
+        while (x, y) != maze.start:
+            maze.maze[y][x].path = True
+            x, y = maze.maze[y][x].parent
+        maze.maze[maze.start[1]][maze.start[0]].path = True
+
+    yield found
+
+def solve_dijkstra(maze):
+    """
+    Solves the given maze using Dijkstra's algorithm.
+    """
+    queue = [(0, maze.start)]
+    distances = {maze.start: 0}
+    found = False 
+
+    while queue:
+        dist, (x, y) = heapq.heappop(queue)
+
+        if (x, y) == maze.end:
+            found = True 
+            break  
+
+        for nx, ny in get_neighbors(maze, x, y):
+            new_dist = dist + 1  # All edges have weight 1
+            if not maze.maze[ny][nx].visited or new_dist < distances[(nx, ny)]:  # Unvisited or shorter path
+                distances[(nx, ny)] = new_dist
+                heapq.heappush(queue, (new_dist, (nx, ny)))
+                maze.maze[ny][nx].visited = True  # Mark as visited
+                maze.maze[ny][nx].parent = x, y
+
+        yield False  # Not yet found a path
+
+    # If a path is found, trace the path
+    if found:
+        x, y = maze.end
+        while (x, y) != maze.start:
+            maze.maze[y][x].path = True
+            x, y = maze.maze[y][x].parent
+        maze.maze[maze.start[1]][maze.start[0]].path = True
+
+    yield found
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from maze import Maze
-from algorithm import solve_bfs
+from algorithm import solve_bfs, solve_dfs, solve_dijkstra
 # Pygame configuration
 WIDTH, HEIGHT = 850, 850  # Window dimensions
 MAZE_WIDTH, MAZE_HEIGHT = 700, 700  # Maze dimensions
@@ -30,6 +30,16 @@ bfs_button_color = (200, 0, 0)  # Red color
 bfs_button_rect = pygame.Rect(701, 60, 100, 50)  # Button position and size
 bfs_generator = None
 
+# Initialize DFS button
+dfs_button_color = (200, 0, 0)  # Red color
+dfs_button_rect = pygame.Rect(701, 120, 100, 50)  # Button position and size
+dfs_generator = None
+
+# Initialize Dijkstra button
+dijkstra_button_color = (200, 0, 0)  # Red color
+dijkstra_button_rect = pygame.Rect(701, 180, 100, 50)  # Button position and size
+dijkstra_generator = None
+
 # Game loop
 running = True
 while running:
@@ -45,12 +55,22 @@ while running:
                 maze = Maze(20, MAZE_WIDTH, MAZE_HEIGHT)  # Create a new empty maze
                 placing_start = False
                 placing_end = False
+                bfs_generator = None  # Clear the BFS generator
+                dfs_generator = None  # Clear the DFS generator
+                dijkstra_generator = None  # Clear the Dijkstra generator
                 no_path_message = "" # Clear the no path message
 
             # Check if the BFS button was clicked
             elif bfs_button_rect.collidepoint(event.pos):
                 bfs_generator = solve_bfs(maze)  # Start the BFS algorithm
-                
+            # Check if the DFS button was clicked
+            elif dfs_button_rect.collidepoint(event.pos):
+                dfs_generator = solve_dfs(maze)  # Start the DFS algorithm
+            # Check if the Dijkstra button was clicked
+            elif dijkstra_button_rect.collidepoint(event.pos):
+                dijkstra_generator = solve_dijkstra(maze)  # Start Dijkstra's algorithm
+
+            # If mouse is outside the maze, ignore the click    
             else:
                 # Convert the mouse position to grid coordinates
                 gx, gy = mx // 20, my // 20
@@ -81,21 +101,24 @@ while running:
             if maze.start != (gx, gy) and maze.end != (gx, gy):
                 maze.maze[gy][gx].wall = True  # set wall attribute to True
 
-
     # Advance the BFS algorithm by one step and redraw the maze
     if bfs_generator is not None:
         try:
-            path = next(bfs_generator)
-            if path is not None:  # Either a path or no path
-                if path:  # A path was found
-                    for x, y in path:
-                        maze.maze[y][x].path = True  # Mark the path cells
-                    bfs_generator = None  # Stop the BFS algorithm
-                else:  # No path was found
-                    no_path_message = "No path found!"
-                    bfs_generator = None  # Clear the BFS generator
+            next(bfs_generator)
         except StopIteration:
-            bfs_generator = None
+            bfs_generator = None  # BFS has finished running
+    # Advance the DFS algorithm by one step and redraw the maze
+    if dfs_generator is not None:
+        try:
+            next(dfs_generator)
+        except StopIteration:
+            dfs_generator = None  # DFS has finished running
+    # Advance the Dijkstra algorithm by one step and redraw the maze
+    if dijkstra_generator is not None:
+        try:
+            next(dijkstra_generator)
+        except StopIteration:
+            dijkstra_generator = None  # Dijkstra has finished running
 
 
     maze.draw(SCREEN)
@@ -120,6 +143,21 @@ while running:
     pygame.draw.rect(SCREEN, bfs_button_color, bfs_button_rect)
     text = font.render('BFS', True, (0, 0, 0))  # Black color
     SCREEN.blit(text, (bfs_button_rect.x + 35, bfs_button_rect.y + 15))  # Position the text
+
+    # Draw the BFS button
+    pygame.draw.rect(SCREEN, bfs_button_color, bfs_button_rect)
+    text = font.render('BFS', True, (0, 0, 0))  # Black color
+    SCREEN.blit(text, (bfs_button_rect.x + 35, bfs_button_rect.y + 15))  # Position the text
+
+    # Draw the DFS button
+    pygame.draw.rect(SCREEN, dfs_button_color, dfs_button_rect)
+    text = font.render('DFS', True, (0, 0, 0))  # Black color
+    SCREEN.blit(text, (dfs_button_rect.x + 35, dfs_button_rect.y + 15))  # Position the text
+
+    # Draw the Dijkstra button
+    pygame.draw.rect(SCREEN, dijkstra_button_color, dijkstra_button_rect)
+    text = font.render('Dijkstra', True, (0, 0, 0))  # Black color
+    SCREEN.blit(text, (dijkstra_button_rect.x + 25, dijkstra_button_rect.y + 15))  # Position the text
 
     # Draw the no path message
     if no_path_message:
